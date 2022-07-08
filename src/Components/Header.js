b2824dd8152e7644bbd2286c12d593dfa50c18de
Dekor129 <95143910+Dekor129@users.noreply.github.com>
2022-07-08 17:55:36 +0300

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Styles/Input.css'
+import './Styles/Header.css'
+
+export default function Header(props) {
+  return (
+    <header>
+        <div className='logo'>TITANIC</div>
+        <div className='input-block'>
+            <input onChange={(event) => props.setTextQuery(event.target.value)} value={props.textQuery} placeholder="Enter the passenger's full name"/>
+            <button 
+              className='btn btn-find' 
+              onClick={() => {
+                props.findPerson( props.textQuery); 
+                props.setBol(true)}
+              }>Find</button>
+            <button 
+            className='btn btn-clear' 
+            onClick={() => {
+              props.showAllPersons(); 
+              props.setBol(false)}
+            }>Clear</button>
+        </div>
+    </header>
+  )
+}
